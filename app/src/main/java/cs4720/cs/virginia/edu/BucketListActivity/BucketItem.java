da6d921876e969add475287d60392b8e817db1bc
Hanna Dickerson <hcd2ha@virginia.edu>
2017-01-30 21:27:50 -0500

    problemas

--- app/src/main/java/cs4720/cs/virginia/edu/BucketListActivity/BucketItem.java
+++ app/src/main/java/cs4720/cs/virginia/edu/BucketListActivity/BucketItem.java
@@ -5,4 +5,41 @@ package cs4720.cs.virginia.edu.BucketListActivity;
  */
 
 public class BucketItem {
+
+    private String name;
+    private String description;
+    private double latitude;
+    private double longitude;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
 }
--- app/src/main/java/cs4720/cs/virginia/edu/BucketListActivity/MainActivity.java
+++ app/src/main/java/cs4720/cs/virginia/edu/BucketListActivity/MainActivity.java
@@ -11,22 +11,61 @@ import android.view.MenuItem;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.R.attr.data;
+
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView RecyclerView;
+    private BucketListAdapter adapter;
     private LinearLayoutManager LinearLayoutManager;
+
+
+    //making the list~~~
+    public static String[] names = {"Streak the Lawn", "Lighting of the Lawn", "High-Five Dean Groves"};
+    public static String[] descs = {"Go run around the homesr statue and come back.", "Have a great time and see some lights",
+        "Just put your hand right on his hand!"};
+    public static double[] lats = {38.0889, 38.0881, 38.0887};
+    public static double[] longs = {-78.5596, -78.5593, -78.5591};
+
+    public static List<BucketItem> getListData(){
+        List<BucketItem> mdata = new ArrayList<>();
+
+        for(int i = 0; i < names.length; i++) {
+            BucketItem item = new BucketItem();
+            item.setName(names[i]);
+            item.setDescription(descs[i]);
+            item.setLongitude(longs[i]);
+            item.setLatitude(lats[i]);
+            mdata.add(item);
+        }
+        return mdata;
+    }
+    //end of list setup - probably should move that????
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //generate items from the activity-main xml
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        List<BucketItem> mdata = getListData();
+        adapter = new BucketListAdapter(mdata, this);
+
+        RecyclerView.setAdapter(adapter);
+
+        //access the RecyclerView from the main activity
         RecyclerView = (RecyclerView) findViewById(R.id.rvContacts);
         LinearLayoutManager = new LinearLayoutManager(this);
         RecyclerView.setLayoutManager(LinearLayoutManager);
 
+        //generate floating action button
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
