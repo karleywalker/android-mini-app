6ed47983f9941b19bf66fb2371251b060dd880f7
Hanna Dickerson <hcd2ha@virginia.edu>
2017-02-06 00:55:01 -0500

    add item back button

--- app/src/main/java/cs4720/cs/virginia/edu/testing/AddItemActivity.java
+++ app/src/main/java/cs4720/cs/virginia/edu/testing/AddItemActivity.java
@@ -8,6 +8,7 @@ import android.app.ActionBar;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,6 +28,11 @@ public class AddItemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+
+        if(getSupportActionBar()!=null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
 
         //onclick method
         Button button = (Button) findViewById(R.id.button);
@@ -52,5 +58,13 @@ public class AddItemActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId() == android.R.id.home){
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
