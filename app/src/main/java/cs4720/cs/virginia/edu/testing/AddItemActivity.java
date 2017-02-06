910b8328e883d930c3737a3279f58716a66e17af
Hanna Dickerson <hcd2ha@virginia.edu>
2017-02-06 00:34:03 -0500

    Add Item Works

--- app/src/main/java/cs4720/cs/virginia/edu/testing/AddItemActivity.java
+++ app/src/main/java/cs4720/cs/virginia/edu/testing/AddItemActivity.java
@@ -5,12 +5,21 @@ package cs4720.cs.virginia.edu.testing;
  */
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
+import java.util.List;
 
+import cs4720.cs.virginia.edu.BucketListActivity.BucketItem;
+import cs4720.cs.virginia.edu.BucketListActivity.EditItemActivity;
+import cs4720.cs.virginia.edu.BucketListActivity.MainActivity;
 import cs4720.cs.virginia.edu.BucketListActivity.R;
+import cs4720.cs.virginia.edu.BucketListActivity.ThingList;
 
 public class AddItemActivity extends AppCompatActivity {
 
@@ -19,12 +28,29 @@ public class AddItemActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
 
-        ActionBar actionBar = getActionBar();
-        ///actionBar.setDisplayHomeAsUpEnabled(true);    //Attempt to invoke virtual method
-            //  'void android.app.ActionBar.setDisplayHomeAsUpEnabled(boolean)'
-            // on a null object reference
+        //onclick method
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //edit in the list of BucketItem
+                List<BucketItem> mdata = ThingList.getMdata();
+                BucketItem b = new BucketItem();
 
-        //getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        //getSupportActionBar().setDisplayShowHomeEnabled(true);
+                EditText edit = (EditText) findViewById(R.id.editText7);
+                b.setName(edit.getText().toString());
+                edit = (EditText) findViewById(R.id.editText8);
+                b.setDescription(edit.getText().toString());
+                edit = (EditText) findViewById(R.id.editText9);
+                b.setLatitude(edit.getText().toString());
+                edit = (EditText) findViewById(R.id.editText10);
+                b.setLongitude(edit.getText().toString());
+
+                mdata.add(b);
+                ThingList.setMdata(mdata);
+                Intent intent = new Intent(AddItemActivity.this, MainActivity.class);
+                startActivity(new Intent(intent));
+            }
+        });
+
     }
 }
